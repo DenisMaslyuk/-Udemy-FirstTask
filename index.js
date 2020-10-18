bc16e5dcc00c6126f3aac373052ad5d351e039f2
DenisMaslyuk <itdenismaslyuk@gmail.com>
2020-10-18 18:04:24 +0700

    3 lessons

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict'
 
-let money = prompt("Ваш бюджет на месяц?", ''),
+let money = +prompt("Ваш бюджет на месяц?", ''),
 	time = prompt('Введите дату в формате YYYY-MM-DD', '');
 
 let appData = {
@@ -12,13 +12,32 @@ let appData = {
 	savings: false
 };
 
-let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
-	a2 = prompt("Во сколько обойдется?", ''),
-	a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
-	a4 = prompt("Во сколько обойдется?", '');
+for (let i=0; i<2;i++){
 
-appData.expenses[a1] = a2;
-appData.expenses[a3] = a4;
+	let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+		b = prompt("Во сколько обойдется?", '');
 
-alert(appData.budget / 30);
+	if (typeof(a)==='string' && typeof(a) !=null && typeof(b) !=null
+		&& a!= '' && b!='' && a.length < 50){
+		console.log("done");
+		appData.expenses[a] = b; 
+	} else {
+		alert("Ввели не верных формат")
+		i--;
+	}
+	
+};
 
+appData.moneyPerDay= appData.budget / 30;
+
+alert("Ежедневный бюджет:" + appData.moneyPerDay)
+
+if(appData.moneyPerDay<100){
+	console.log("Минимальный уровень достатка");
+} else if (appData.moneyPerDay > 100 && 2000){
+	console.log("Средний уровень достатка");
+}else if(appData.moneyPerDay >2000){
+	console.moneyPerDay("Высокий уровень достатка")
+} else {
+	console.log("Произошла огибка")
+}
